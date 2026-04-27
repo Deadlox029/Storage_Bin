265657dfad2beb78216005710df74df3546de834
Dean Deist <dean.deist@trojans.dsu.edu>
2026-04-27 10:27:42 -0500

    added checking for duplicate lines, and other modifications.

--- main_maybe.c
+++ main_maybe.c
@@ -75,10 +75,55 @@ void longest(struct head_t *S){
 
     printf("%d %d\n", longest, shortest);
 }
+//duplicate and unique lines
+void dup_uniq_lines(struct head_t *S) {
+    int n = S->lines;
+    struct text_t nodes[n];
 
+    struct text_t *cur = S->link;
+    for (int i = 0; i<n; i++) {
+        nodes[i] = *cur;
+        cur = cur->link;
+    }
+
+    int visited[n];
+    for (int i = 0; i<n; i++)
+        visited[i] = 0;
+
+    int unique = 0;
+    int duplicate = 0;
+
+    for (int i = 0; i<n; i++) {
+        if(visited[i] == 1)
+            continue; //skipping read lines
+
+        int count = 1; //number of occurances
+
+        for (int next = i+1; next<n; next++) {
+            if (strcmp(nodes[i].text, nodes[next].text) == 0) { //checking for duplicates
+                duplicate = duplicate + 1; //increasing number of occurances of duplicate
+                //printf("dupe found!\n");
+                visited[next]=1; //marking line as read
+            }
+            else
+            {
+                //printf("No dupe here!\n");
+            }
+        }
+
+        unique++;
+        if (count>1)
+            duplicate++;
+    }
+
+    printf("Number of unique lines = %d\n", unique);
+    printf("Number of duplicate lines = %d\n", duplicate);
+}
 
 int main(int argc, char *argv[]) {
     FILE *fp = fopen(argv[1],"r");
+    if (fp == NULL)
+        return -1;
     struct head_t *storage = malloc(sizeof(struct head_t));
     storage->lines = 0;
     storage->link = NULL;
@@ -89,4 +134,5 @@ int main(int argc, char *argv[]) {
     if (Lflag==0)
         printf("Last Line is NOT duplicated\n");
     longest(storage);
+    dup_uniq_lines(storage);
 }
